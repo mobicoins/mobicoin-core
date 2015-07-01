4823def410b4cdf0951f5ef0566804b7201e4ccd
yezune <yezune@gmail.com>
2015-07-01 17:20:51 +0900

    Update value display unit

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -116,7 +116,7 @@ void setupAmountWidget(QLineEdit *widget, QWidget *parent)
 bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 {
     // return if URI is not valid or is no mobicoin: URI
-    if(!uri.isValid() || uri.scheme() != QString("Mobicoin"))
+    if(!uri.isValid() || uri.scheme() != QString("mobicoin"))
         return false;
 
     SendCoinsRecipient rv;
@@ -176,9 +176,9 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
     //
     //    Cannot handle this later, because mobicoin:// will cause Qt to see the part after // as host,
     //    which will lower-case it (and thus invalidate the address).
-    if(uri.startsWith("Mobicoin://", Qt::CaseInsensitive))
+    if(uri.startsWith("mobicoin://", Qt::CaseInsensitive))
     {
-        uri.replace(0, 11, "Mobicoin:");
+        uri.replace(0, 11, "mobicoin:");
     }
     QUrl uriInstance(uri);
     return parseBitcoinURI(uriInstance, out);
@@ -186,7 +186,7 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
 
 QString formatBitcoinURI(const SendCoinsRecipient &info)
 {
-    QString ret = QString("Mobicoin:%1").arg(info.address);
+    QString ret = QString("mobicoin:%1").arg(info.address);
     int paramCount = 0;
 
     if (info.amount)
